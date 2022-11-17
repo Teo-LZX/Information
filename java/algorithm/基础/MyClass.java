5b0595e9beaec0c4282b6caf37824461d9945f0e
mercury <Mercury>
2022-11-17 20:52:22 +0800

    test

--- java/algorithm/基础/MyClass.java
+++
@@ -1,7 +0,0 @@
-package java.algorithm.基础;
-
-public class MyClass {
-    public static void main(String[] args) {
-      System.out.println("Hello World");
-    }
-  }
--- java/algorithm/基础/P1026.java
+++
@@ -1,5 +0,0 @@
-public class P1026{
-    public static void main(String[] args) {
-        System.out.println('hello world');
-    }
-}
